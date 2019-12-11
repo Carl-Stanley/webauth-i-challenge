984cb3118d331e81dae298cdf00493f368a605f8
Carl-Stanley <cds.beml@gmail.com>
2019-12-10 19:12:40 -0700

    Commit

--- api/usersrouter.js
+++ api/usersrouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const Projects = require('./usersmodel.js');
+const Users = require('./usersmodel.js');
 
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    Projects.find().then(users => {
-        res.status(200).json(users.map(users.changeCompletedProperty));
+    Users.find().then(users => {
+        res.status(200).json(users.map(Users.changeCompletedProperty));
     }).catch(err => {
         console.log(err);
         res.status(500).json({ message: 'error getting users' });
